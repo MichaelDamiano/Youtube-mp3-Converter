dac5ae74f56769ba66f654b8f5befb78cc6a6bcb
MichaelDamiano <48996530+MichaelDamiano@users.noreply.github.com>
2020-12-29 21:30:15 -0500

    Update background.js
    added comments

--- background.js
+++ background.js
@@ -1,41 +1,43 @@
 var url
+//begins execution of app.js
 chrome.browserAction.onClicked.addListener(function(tab) {
     chrome.tabs.executeScript(null, {file: "/app.js"});
 });
 
- chrome.extension.onConnect.addListener(function(port) 
- {
-      port.onMessage.addListener(function(msg) {
-          if (port.name == "download")
-          {
-              port.postMessage(url);
-          }
-          else
-          {
-            url = msg;
-            setTimeout(getTab, 3000, port, msg); 
-          }
-      });
+//A listener for two different ports. 
+chrome.extension.onConnect.addListener(function(port) 
+{
+    port.onMessage.addListener(function(msg) {
+    if (port.name == "download")
+    {
+        //send the url of the youtube video to download.js
+        port.postMessage(url);
+    }
+    else
+    {
+        //we must save msg into a variable because we will use this to send the url to the other port in the if statement above
+        url = msg;
+        setTimeout(getTab, 3000, port, url); 
+    }
+    });
  });
 
-
-
-
-function getTab(port, msg) {
-    var tabID;
-
-  chrome.tabs.query({}, function(tabs) 
-  {
-    for (var i = 0; i < tabs.length; i++)
+//switch the active tab from youtube to ytmp3
+function getTab(port, msg) 
+{
+    chrome.tabs.query({}, function(tabs) 
     {
-        if(tabs[i].url.includes('https://ytmp3.cc'))
+        //iterate through all the tabs. stop when we reach the ytmp3 tab
+        for (var i = 0; i < tabs.length; i++)
         {
-            tabID = tabs[i].id;
-            chrome.tabs.update(tabs[i].id, {selected: true});
-            break;
+            if(tabs[i].url.includes('https://ytmp3.cc'))
+            {
+                chrome.tabs.update(tabs[i].id, {selected: true});
+                break;
+            }
         }
-    }
-    chrome.tabs.executeScript(tabID, {file: "/download.js"});
+        //start the download
+        chrome.tabs.executeScript(tabID, {file: "/download.js"});
   });
 }
 
