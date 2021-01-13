6e0a58731a26534c892b0af807f7c1d9bb98c3ae
Michael Damiano <48996530+MichaelDamiano@users.noreply.github.com>
2021-01-12 21:09:45 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,5 @@
-
- var url; 
-
-
- var port = chrome.extension.connect({name: "youtube"});
+var url; 
+var port = chrome.extension.connect({name: "youtube"});
 url = window.location.href;
 window.open('https://ytmp3.cc/');
 port.postMessage(url);
-
-port.onMessage.addListener(function(msg) {
-  alert(msg);
-});
