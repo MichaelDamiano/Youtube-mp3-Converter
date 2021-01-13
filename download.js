65b9f1c3b29933ed300a5a1db0e9203749b56459
Michael Damiano <48996530+MichaelDamiano@users.noreply.github.com>
2021-01-12 21:13:25 -0500

    Update download.js

--- download.js
+++ download.js
@@ -1,18 +1,24 @@
  var port = chrome.extension.connect({name: "download"});
- var notStarted = true;
+ var interval;
  port.postMessage("");
  
  port.onMessage.addListener(function(msg) {
    document.getElementById('input').value = msg;
    document.getElementById('submit').click();
-   setInterval(download, 1000);
+   interval = setInterval(download, 1000);
 });
 
 function download(){
-   if (document.getElementById('title').innerHTML.trim() != 'Please insert a valid video URL' && notStarted)
-   {
-       notStarted = false;
-       document.getElementById('buttons').children[0].click();
-       setTimeout(function(){ close(); }, 1000);
+    try{
+        if (document.getElementById('title').innerHTML.trim() != 'Please insert a valid video URL')
+        {
+            clearInterval(interval);
+            document.getElementById('buttons').children[0].click();
+        }
+    }
+   catch(err) {
+       clearInterval(interval);
+       alert("An error has occured, download failed. Please try again.");
    }
+   finally{setTimeout(function(){ close(); }, 1000);}
 }
